98a0e0191e38815667bff8e547630aea2ca782c1
Jack Dawson <jack.dawson@uniteldirect.co.uk>
2025-09-03 16:07:08 +0100

    wip

--- app/helpers/public_websites_helper.rb
+++ app/helpers/public_websites_helper.rb
@@ -1,6 +1,43 @@
 module PublicWebsitesHelper
 
-  def test_data(data)
-    data
+  def render_show_content(component, user_id = nil, theme_page_id = nil, component_page_id)
+    componentHTML = component.content['html']
+    updated_content = componentHTML
+
+    unless component.editable_fields == ""
+      # Get customisations if user_id and theme_page_id are provided
+      field_values = get_component_field_values(component, user_id, theme_page_id, component_page_id)
+
+      field_values.each do |field_name, field_value|
+        updated_content = updated_content.gsub('{{'+field_name.to_s+'}}', field_value.to_s)
+      end
+    end
+
+    if updated_content.include?('{{nav_items}}')
+      unless component.template_patterns == ""
+        nav_items_html = render_navbar_items(component)
+        updated_content = updated_content.gsub!('{{nav_items}}', nav_items_html)
+      end
+    end
+
+    if updated_content.include?('{{service_items}}')
+      unless component.template_patterns == ""
+        service_items_html = render_service_items(component)
+        updated_content = updated_content.gsub!('{{service_items}}', service_items_html)
+      end
+    end
+
+    if component.component_type == 'Service Inner'
+      service = current_user.website.services.find { |s| s["id"] == theme_page_id }
+      updated_content = updated_content.gsub!('{{service_title}}', service['name'])
+      updated_content = updated_content.gsub!('{{service_content}}', simple_format(service['content']))
+      updated_content = updated_content.gsub!('{{service_image}}', service['featured_image'])
+    elsif component.component_type == 'Blog Inner'
+
+    elsif component.component_type == 'Product Inner'
+
+    end
+
+    updated_content
   end
 end
