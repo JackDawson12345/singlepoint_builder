f9091caef591e4608cc309d6f2ec99731ddad592
Jack Dawson <jack.dawson@uniteldirect.co.uk>
2025-09-09 10:40:16 +0100

    wip

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,8 @@ module SinglepointBuilder
     # Add this to config/application.rb in the Application class
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.active_record.encryption.primary_key = Rails.application.credentials.active_record_encryption[:primary_key]
-    config.active_record.encryption.deterministic_key = Rails.application.credentials.active_record_encryption[:deterministic_key]
-    config.active_record.encryption.key_derivation_salt = Rails.application.credentials.active_record_encryption[:key_derivation_salt]
-
+    config.active_record.encryption.primary_key = Rails.application.credentials.active_record_encryption[:primary_key] || ENV['ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY']
+    config.active_record.encryption.deterministic_key = Rails.application.credentials.active_record_encryption[:deterministic_key] || ENV['ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY']
+    config.active_record.encryption.key_derivation_salt = Rails.application.credentials.active_record_encryption[:key_derivation_salt] || ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
   end
 end
